bd5f34ef03be6c6accdb1112961d7bb8b21cfe05
iris-personal <mary.iris.toland@gmail.com>
2022-08-08 12:44:08 -0500

    crystal model and migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Crystal(models.Model):
+    type = models.CharField(max_length=50)
+    colors = models.CharField(max_length=100)
+    properties = models.TextField(max_length=250)
