a5351c5b155721a0828826240d416552147676ad
iris-personal <mary.iris.toland@gmail.com>
2022-08-08 13:05:28 -0500

    individual details page included

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Crystal
 
-# Register your models here.
+admin.site.register(Crystal)
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,6 @@ def about(request):
 def crystals_index(request):
   crystals = Crystal.objects.all()
   return render(request, 'crystals/index.html', { 'crystals': crystals })
+def crystals_detail(request, crystal_id):
+  crystal = Crystal.objects.get(id=crystal_id)
+  return render(request, 'crystals/detail.html', { 'crystal': crystal })
